faf4b49107a8d9040576c7f70acb3427e41033f4
f00d <delossantos.orion@student.greenriver.edu>
2026-05-09 13:22:04 -0700

    beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import { useState } from 'react'
-import './App.css'
+
+const gridSize = 16;
+const defaultColor = '#ff0000'
+
+function makeGrid(){
+  return Array.from({
+    length: gridSize },
+    () => Array(gridSize).fill(defaultColor))
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [grid, setGrid] = useState(makeGrid)
+  const [color, setColor] = useState(defaultColor)
 
+  
   return (
     <>
-      <h1>Hi</h1>
+      <h1>Pixel Art Editor</h1>
+
+      <div id="colorSelector">
+        <h3>Color</h3>
+        <div id="currentColor"></div>
+        <div id="colorOptions"></div>
+        <button id="clearButton" onClick={() => (
+          setGrid([...grid, 1],
+          console.log({grid})
+          ))}>Clear</button>
+        
+        <p>Click any cell to paint it with the current color.</p>
+
+      </div>
     </>
   )
 }
